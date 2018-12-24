e3c66429d02e8fc1447d71c30aba051168563fc2
ZYL <1610252266@vip.henu.edu.cn>
2018-12-24 18:23:28 +0800

    功能数据管理

---
+++ src/main/java/com/project/trans/Service/DataService.java
@@ -0,0 +1,11 @@
+package com.project.trans.Service;
+
+import com.project.trans.Bean.Data;
+
+import java.util.List;
+
+public interface DataService {
+    public List<Data> selectall();
+
+    public List<Data> selectdata(Integer tId);
+}
